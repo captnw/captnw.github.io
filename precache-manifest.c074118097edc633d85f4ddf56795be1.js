299b068f754c20f27bb9b1d7a263a186ad7228dd
captnw <williamngoc93@hotmail.com>
2020-09-20 14:34:23 -0700

    Updates

---
+++ precache-manifest.c074118097edc633d85f4ddf56795be1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a0b48b2bc805908d5dd7d6fcdd4e65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a2ccc4e90812cfdd0bd",
+    "url": "/static/css/main.3a43934d.chunk.css"
+  },
+  {
+    "revision": "45710bd079bbd9d06f67",
+    "url": "/static/js/2.0585ef59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0585ef59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a2ccc4e90812cfdd0bd",
+    "url": "/static/js/main.eb775825.chunk.js"
+  },
+  {
+    "revision": "2a752d2667d01594e6f1",
+    "url": "/static/js/runtime-main.229f073a.js"
+  },
+  {
+    "revision": "6b22e480b3fb251325c9d9f3a7fe53b9",
+    "url": "/static/media/MoneyPay_1.6b22e480.jpg"
+  },
+  {
+    "revision": "ef860724018f5984e897cb2813589ee2",
+    "url": "/static/media/OldResume.ef860724.pdf"
+  },
+  {
+    "revision": "cfa3b893dcbdfe3fd1aad942b6f12543",
+    "url": "/static/media/SimpleDungeon_1.cfa3b893.jpg"
+  }
+]);
