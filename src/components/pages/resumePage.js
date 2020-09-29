50576f6c79bfbbc7669729fceaacb9d13a3e4fc7
captnw <williamngoc93@hotmail.com>
2020-09-28 17:37:50 -0700

    Attempt to fix resume not displaying on mobile devices

--- src/components/pages/resumePage.js
+++ src/components/pages/resumePage.js
@@ -1,10 +1,20 @@
 import React from "react";
-import resumePdf from "../../Assets/files/MyResume.pdf"
+import resumePdf from "../../Assets/files/MyResume.pdf";
 
 function ResumePage() {
   return (
-    <div className="container whiteBackground">
-      <embed src={resumePdf} type="application/pdf" width="100%" height="900vh" />
+    <div className="container whiteBackground horizontalCenter">
+      <object
+        data={resumePdf}
+        type="application/pdf"
+        width="100%"
+        height="900vh"
+      >
+        <p>
+          Your web browser doesn't have a PDF plugin. Instead you can
+          <a href={resumePdf}>click here to download the pdf file.</a>
+        </p>
+      </object>
     </div>
   );
 }
